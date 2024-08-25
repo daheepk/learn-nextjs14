cb513ed87038eea0beb520d5ddd54cf23e92a196
Dahee Park <ekgml3219@gmail.com>
2024-08-25 22:32:09 +0900

    feat: add movie-videos, movie-info

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,7 @@
-import Navigation from "./components/navigation";
+import Navigation from "../components/navigation";
 
 export default function NotFound(){
     return (<div>
-        <Navigation />
         <h1>Not found!</h1>
     </div>);
 }
